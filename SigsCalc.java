07a2a0912fe970eba4e8985d9f9a6e7a98cf0139
Zygmunt Mocek <zygmunt.mocek@gmail.com>
2020-05-27 19:41:02 +0200

    Update SigsCalc.java
    Small housekeeping changes.

--- SigsCalc.java
+++ SigsCalc.java
@@ -2,17 +2,13 @@ import javax.swing.JFrame;
 
 public class SigsCalc {
 
-	public SigsCalc() {
-		// TODO Auto-generated constructor stub
-	}
-
 	//Shows the window
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		SigsCalcWindow window = new SigsCalcWindow();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setSize(400, 400);
+		window.setSize(600, 600);
 		window.setVisible(true);
 
 	}
